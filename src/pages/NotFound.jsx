fb4dc8f7283dd061cb0386532fe80457fa238b8f
Maxim <kejerino@yandex.ru>
2022-05-28 23:32:40 +0800

    feat!: created page error 404

---
+++ src/pages/NotFound.jsx
@@ -0,0 +1,11 @@
+import image from "../images/error404.svg"
+
+function NotFound() {
+    return(
+        <div className="noteFound">
+            <img className="noteFound__image" src={image} alt=""/>
+        </div>
+    )
+}
+
+export default NotFound;
