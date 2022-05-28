e1727c2819f2b27c3b7b58669b89b87f1540a26f
Maxim <kejerino@yandex.ru>
2022-05-28 23:34:34 +0800

    feat!: added change state for PurchasePopup

--- src/components/PurchasePopup.jsx
+++ src/components/PurchasePopup.jsx
@@ -1,4 +1,5 @@
 import PopupWithForm from "./PopupWithForm";
+import OrderButton from "./OrderButton";
 
 function PurchasePopup(props) {
     return (
@@ -7,27 +8,36 @@ function PurchasePopup(props) {
             onClose={props.onClose}
             name={"purchase-popup"}
         >
-            <div className="__wrapper">
-                <div className="preview">
-                    <img className="rating" src=""/>
-                    <img className="image-preview"/>
+            <div className="purchase-popup">
+                <div className="purchase-popup__preview">
+                    <img className="purchase-popup__rating" src=""/>
+                    <img className="purchase-popup__image-preview" src={'https://www.svgrepo.com/show/124413/burger.svg'}/>
                 </div>
-                <div className="">
-
+                <div className="purchase-popup__wrapper">
+                    <h2 className="purchase-popup__title">Саларианcкий малыш</h2>
+                    <p className="purchase-popup__description">Ветчина, полукопченая колбаса,
+                        болгарский перец, маринованные огурцы,
+                        белый соус, сыр моцарелла, зелень</p>
+                    <nav className="purchase-popup__constructor">
+                        <ul className="purchase-popup__category">
+                            <li className="active">Острый</li>
+                            <li>Нормальный</li>
+                        </ul>
+                        <ul className="purchase-popup__category">
+                            <li>120гр.</li>
+                            <li className="active">200гр.</li>
+                            <li>370гр.</li>
+                        </ul>
+                    </nav>
+                    <div className="purchase-popup__price-wrapper">
+                        <div className="purchase-popup__price">{props.price} &psi;</div>
+                        <OrderButton
+                            name={'В корзину'}
+                            addIconButton={true}
+                            count={0}
+                        />
+                    </div>
                 </div>
-                <h2 className="__title">название позиции</h2>
-                <p className="__description">description</p>
-                <nav>
-                    <ul>
-                        <li className="active">Острый</li>
-                        <li>Нормальный</li>
-                    </ul>
-                    <ul>
-                        <li>120гр.</li>
-                        <li className="active">200гр.</li>
-                        <li>370гр.</li>
-                    </ul>
-                </nav>
             </div>
         </PopupWithForm>
     )
