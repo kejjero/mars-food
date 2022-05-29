09c842ba4ad0d272148b19577ef5c078c7f107d2
Maxim <kejerino@yandex.ru>
2022-05-29 23:02:12 +0800

    fix: fixed params skeleton

--- src/components/ItemsBlock/Skeleton.jsx
+++ src/components/ItemsBlock/Skeleton.jsx
@@ -3,19 +3,19 @@ import ContentLoader from "react-content-loader"
 
 const Skeleton = () => (
     <ContentLoader
-        className="pizza-block"
+        className="skeleton"
         speed={2}
-        width={260}
-        height={370}
+        width={240}
+        height={380}
         viewBox="0 0 260 370"
         backgroundColor="#3b0a6c"
         foregroundColor="#4a1084"
     >
-        <circle cx="129" cy="118" r="78"/>
-        <rect x="39" y="207" rx="10" ry="10" width="180" height="30"/>
-        <rect x="0" y="333" rx="7" ry="7" width="87" height="30"/>
-        <rect x="150" y="327" rx="20" ry="20" width="110" height="40"/>
-        <rect x="1" y="252" rx="12" ry="12" width="260" height="63"/>
+        <circle cx="130" cy="80" r="78"/>
+        <rect x="40" y="195" rx="10" ry="10" width="180" height="30"/>
+        <rect x="0" y="335" rx="7" ry="7" width="87" height="30"/>
+        <rect x="150" y="330" rx="20" ry="20" width="110" height="40"/>
+        <rect x="0" y="242" rx="12" ry="12" width="260" height="73"/>
     </ContentLoader>
 )
 
