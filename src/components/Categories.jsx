df90b17e64edcbb30c10a2e0279ec3fe81b62ff3
Maxim <kejerino@yandex.ru>
2022-06-08 15:37:18 +0800

    feat: added state categoryId with Redux

--- src/components/Categories.jsx
+++ src/components/Categories.jsx
@@ -3,26 +3,35 @@ import iconBurger from '../images/icon-burger.png'
 import iconLasagna from '../images/icon-lasagna.png'
 import iconJuice from '../images/icon-juice.png'
 import iconFork from '../images/icon-fork.png'
+import {useSelector, useDispatch} from "react-redux";
+import {setCategoryId} from "../redux/slices/filterSlice";
 
-function Categories(props) {
+const categories  = [
+    {name: 'Все', img: iconFork},
+    {name: 'Бургеры', img: iconBurger},
+    {name: 'Пицца', img: iconPizza},
+    {name: 'Десерты', img: iconLasagna},
+    {name: 'Напитки', img: iconJuice},
+]
 
-    const categories  = [
-        {name: 'Все', img: iconFork},
-        {name: 'Бургеры', img: iconBurger},
-        {name: 'Пицца', img: iconPizza},
-        {name: 'Десерты', img: iconLasagna},
-        {name: 'Напитки', img: iconJuice},
-    ]
+function Categories({ getCountPages }) {
+    const categoryId = useSelector((state) => state.filterReducer.categoryId)
+    const dispatch = useDispatch()
+
+    function onClickCategory(index) {
+        dispatch(setCategoryId(index))
+        getCountPages(index)
+    }
 
     return (
         <div className="categories">
             <ul>{
-                categories.map((category , i) => {
+                categories.map((category , index) => {
                     return (
                         <li
-                            key={i}
-                            className={props.activeIndex === i ? "active" : ""}
-                            onClick={() => props.handleIndexMenu(i)}
+                            key={index}
+                            className={categoryId === index ? "active" : ""}
+                            onClick={() => onClickCategory(index)}
                         >
                             <img src={category.img} alt="category"/>
                             <h2>{category.name}</h2>
