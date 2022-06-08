6f1ea736fbb702f247b8fa5d25be4f7a008ef6fe
Maxim <kejerino@yandex.ru>
2022-06-08 15:35:53 +0800

    feat: added get pageCount with request to mockapi

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -7,10 +7,14 @@ import ReactPaginate from "react-paginate";
 import styles from "../scss/modules/pagination.module.scss"
 import {useContext} from "react";
 import {SearchContext} from "../components/App";
+import {useDispatch} from "react-redux";
+import {setCurrentPage, setPageCount} from "../redux/slices/filterSlice";
+import axios from "axios";
 
 function Home(props) {
+    const dispatch = useDispatch();
     const {searchValue} = useContext(SearchContext);
-
+    const skeleton = [...new Array(4)].map((_, i) => <Skeleton key={i}/>)
     const items = props.items.filter(obj => {
         return obj.title.toLowerCase().includes(searchValue.toLowerCase())
     }).map((item) => {
@@ -25,22 +29,27 @@ function Home(props) {
         )
     })
 
-    const skeleton = [...new Array(4)].map((_, i) => <Skeleton key={i}/>)
+    // Пришлось сделать костыль, так как бэкенд не дает количество страниц для корректной пагинации
+    function getCountPages (index) {
+        const request = index !== 0 ? index : ''
+        axios.get(`https://6291e4289d159855f081d72e.mockapi.io/items?category=${request}`)
+            .then((res) => countItems(res.data))
+    }
+
+    function countItems(items) {
+        const mathPages = Math.ceil(items.length / 4)
+        dispatch(setPageCount(mathPages))
+    }
 
     return (
         <>
             <div className="content__top">
                 <img className="content__ufo-menu" src={ufoMenu} alt=""/>
-                <Categories
-                    handleIndexMenu={props.handleIndexMenu}
-                    activeIndex={props.activeIndex}
-                />
+                <Categories getCountPages={getCountPages}/>
             </div>
             <div className="content__wrapper-title">
                 <h2 className="content__title">Меню</h2>
-                <Sort
-                    handleActiveSort={props.handleActiveSort}
-                />
+                <Sort/>
             </div>
             <div className="content__items">
                 {props.isLoading ? skeleton : items}
@@ -49,9 +58,9 @@ function Home(props) {
                 className={styles.pagination}
                 breakLabel="..."
                 nextLabel="→"
-                onPageChange={(evt) => props.onChangePage(evt.selected + 1)}
+                onPageChange={(evt) => dispatch(setCurrentPage(evt.selected))}
                 pageRangeDisplayed={4}
-                pageCount={3}
+                pageCount={props.pageCount}
                 previousLabel="←"
                 renderOnZeroPageCount={null}
             />
