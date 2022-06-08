c32d48aebd2fd5106ae2845ae1a2a6859dca7657
Maxim <kejerino@yandex.ru>
2022-06-08 15:25:36 +0800

    feat!: installed packages: redux, toolkit, debounce, qs

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,18 +1,26 @@
 import '../scss/app.scss'
 import Header from "./Header";
-import {useState, useEffect, createContext} from "react";
-import {Routes, Route} from "react-router-dom";
+import {useState, useEffect, createContext, useRef} from "react";
+import {Routes, Route } from "react-router-dom";
 import PurchasePopup from "./PurchasePopup";
 import Footer from "./Footer"
 import Home from "../pages/Home";
 import NotFound from "../pages/NotFound";
 import Cart from "../pages/Cart";
 import backgorundSpace from "../images/background_space.svg"
-import {useLocation} from "react-router";
+import {useLocation, useNavigate} from "react-router";
+import {useSelector, useDispatch} from "react-redux";
+import axios from "axios";
+import qs from "qs"
+import {setFilters} from "../redux/slices/filterSlice";
 
 export const SearchContext = createContext('');
 
 function App() {
+    const location = useLocation();
+    const navigate = useNavigate();
+    const dispatch = useDispatch();
+
     const [isPurchasePopupOpen, setIsPurchasePopupOpen] = useState(false);
     const [items, setItems] = useState([]);
     const [isLoading, setIsLoading] = useState(true);
@@ -20,36 +28,54 @@ function App() {
     const [pricePopup, setPricePopup] = useState(0)
     const [imagePopup, setImagePopup] = useState('')
     const [purchaseCounter, setPurchaseCounter] = useState(0)
-    const location = useLocation();
-    const [activeIndex, setActiveIndex] = useState(0);
-    const [sortCategory, setSortCategory] = useState('');
-    const [sortType, setSortType] = useState(`sortBy=rating&order=desc`)
-    const [currentPage, setCurrentPage] = useState(1)
     const [searchValue, setSearchValue] = useState('');
+    const {categoryId, sort, currentPage, pageCount} = useSelector((state) => state.filterReducer)
+
+    const isSearch = useRef(false);
+    const isCategory = categoryId !== 0 ? categoryId : ''
+    const isSort = `sortBy=${sort.property}&order=${sort.type}`
+    const filterRequest = `category=${isCategory}&${isSort}&page=${currentPage}&limit=4`
+
     useEffect(() => {
-        fetch(`https://6291e4289d159855f081d72e.mockapi.io/items?category=${sortCategory}&${sortType}&page=${currentPage}&limit=4`)
-            .then((res) => {
-                return res.json()
-            })
-            .then((data) => {
-                setItems(data)
-                setIsLoading(false)
-            })
-    },[sortCategory, sortType, currentPage])
-
-    function handleActiveCategory(index) {
-        setActiveIndex(index)
-        if (index === 0) {
-            setSortCategory('')
-        } else {
-            setSortCategory(index)
+        if(window.location.search){
+            const params = qs.parse(window.location.search.substring(1))
+            dispatch(setFilters(params))
         }
+        isSearch.current = false;
+    }, [])
+
+    function fetchItems() {
+        axios.get(`https://6291e4289d159855f081d72e.mockapi.io/items?${filterRequest}`)
+            .then((res) => {
+                if(res.status === 200) {
+                    setItems(res.data)
+                    setIsLoading(false)
+                }
+                else {
+                    alert('Статус ошибки: ' + res.status)
+                }
+            })
+            .catch((err) => alert('Статус ошибки: ' + err))
     }
 
-    function handleActiveSort(sortProperty, sortType) {
-        const sortingRequest = `sortBy=${sortProperty}&order=${sortType}`
-        setSortType(sortingRequest)
-    }
+    useEffect(() => {
+        if(!isSearch.current) {
+            fetchItems();
+        }
+        isSearch.current = false;
+
+    },[filterRequest])
+
+    useEffect(() => {
+        const queryString = qs.stringify({
+            category: categoryId,
+            property: sort.property,
+            order: sort.type,
+            sortId: sort.sortId,
+            page: currentPage
+        })
+        navigate(`?${queryString}`)
+    }, [categoryId, currentPage, navigate, sort.property, sort.sortId, sort.type])
 
     function handlePurchasePopup(title, price, image) {
         setIsPurchasePopupOpen(true)
@@ -84,10 +110,7 @@ function App() {
                                   items={items}
                                   isLoading={isLoading}
                                   handlePurchasePopup={handlePurchasePopup}
-                                  handleIndexMenu={handleActiveCategory}
-                                  activeIndex={activeIndex}
-                                  handleActiveSort={handleActiveSort}
-                                  onChangePage={(number) => setCurrentPage(number)}
+                                  pageCount={pageCount}
                               />
                           }/>
                           <Route path="/cart" element={<Cart/>}/>
