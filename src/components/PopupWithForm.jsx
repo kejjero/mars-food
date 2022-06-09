0c9bdd3f3253dcbef35e38be5dc16be30f07fc87
Maxim <kejerino@yandex.ru>
2022-06-09 22:09:32 +0800

    feat!: added dispatch for close & open popups

--- src/components/PopupWithForm.jsx
+++ src/components/PopupWithForm.jsx
@@ -1,23 +1,29 @@
-function PopupWithForm({isOpen, name, title, children, onClose, onSubmit}) {
+import {useDispatch, useSelector} from "react-redux";
+import {closeAllPopups} from "../redux/slices/popupWithFormSlice";
+
+function PopupWithForm({children, onSubmit}) {
+    const dispatch = useDispatch();
+    const popup = useSelector(state => state.popupWithFormReducer)
+
     return (
-        <div className={`popup popup_${name} ${isOpen && 'popup_opened'}`}>
+        <div className={`popup popup_${popup.name} ${popup.isOpen && 'popup_opened'}`}>
             <div className="popup__container">
                 <button
                     type="button"
                     className="popup__close-button"
                     aria-label="Закрыть"
-                    onClick={onClose}
+                    onClick={() => dispatch(closeAllPopups())}
                 >
                 </button>
                 <form
                     className={"popup__form"}
                     method={"post"}
-                    name={name}
+                    name={popup.name}
                     onSubmit={onSubmit}
                 >{children}
                 </form>
             </div>
-            <div className="popup__overlay" onClick={onClose}>
+            <div className="popup__overlay" onClick={() => dispatch(closeAllPopups())}>
             </div>
         </div>
     )
