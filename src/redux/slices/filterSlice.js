d55a91f36d767d30aa4c8d0fe60257490e403cd9
Maxim <kejerino@yandex.ru>
2022-06-11 23:30:57 +0800

    refactor: refactored name actions & added selectors

--- src/redux/slices/filterSlice.js
+++ src/redux/slices/filterSlice.js
@@ -1,7 +1,8 @@
-import { createSlice } from "@reduxjs/toolkit"
+import {createSlice} from "@reduxjs/toolkit"
 
 const initialState = {
     categoryId: 0,
+    searchValue: '',
     sort: {
         sortId: 0,
         property: 'rating',
@@ -15,28 +16,33 @@ export const filterSlice = createSlice({
     name: 'filter',
     initialState,
     reducers: {
-        setCategoryId(state, activeId) {
-            state.categoryId = activeId.payload;
+        setCategoryId(state, action) {
+            state.categoryId = action.payload;
         },
-        setSort(state, obj) {
-            state.sort = obj.payload
+        setSort(state, action) {
+            state.sort = action.payload
         },
-        setCurrentPage(state, currentPage) {
-            state.currentPage = currentPage.payload + 1
+        setSearchValue(state, action) {
+            state.searchValue = action.payload;
+        },
+        setCurrentPage(state, action) {
+            state.currentPage = action.payload + 1
         },
         setPageCount(state, count) {
             state.pageCount = count.payload
         },
-        setFilters(state, obj) {
-            state.categoryId = Number(obj.payload.category)
-            state.sort.sortId = Number(obj.payload.sortId)
-            state.sort.property = obj.payload.property
-            state.sort.type = obj.payload.order
-            state.currentPage = Number(obj.payload.page)
+        setFilters(state, action) {
+            state.categoryId = Number(action.payload.category)
+            state.sort.sortId = Number(action.payload.sortId)
+            state.sort.property = action.payload.property
+            state.sort.type = action.payload.order
+            state.currentPage = Number(action.payload.page)
         }
     }
 })
 
-export const { setCategoryId, setSort, setCurrentPage, setPageCount, setFilters } = filterSlice.actions;
-
+export const {setCategoryId, setSort, setCurrentPage, setPageCount, setFilters, setSearchValue} = filterSlice.actions;
+export const selectCategoryId = (state) => state.filterReducer.categoryId;
+export const selectFilter = (state) => state.filterReducer;
+export const selectSearchValue = (state) => state.filterReducer.searchValue;
 export default filterSlice.reducer;
